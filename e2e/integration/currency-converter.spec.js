daa76036f1247e3ad60858f76e0d6c364ba8e4e5
Jonathan <jonathanwu916@gmail.com>
2020-11-08 23:28:45 +1300

    implement: currency converter page e2e test

--- e2e/integration/currency-converter.spec.js
+++ e2e/integration/currency-converter.spec.js
@@ -11,15 +11,47 @@ describe('Currency Converter Page', () => {
         cy.url().should('include', '/converter');
 
         const topbarConverterTab = cy.get('.top-bar__nav.converter');
-        topbarConverterTab.should('contain.text', 'Currency Converter');
+        topbarConverterTab.contains('Currency Converter');
         topbarConverterTab.should('have.class', 'active');
     });
 
-    it('should by default use NZD as "From" currency and USD as "To" currency and 1 as Amount', () => {});
+    it('should by default use NZD as "From" currency and USD as "To" currency and 1 as Amount', () => {
+        cy.get('input[ng-reflect-placeholder=From]')
+            .invoke('val')
+            .then(text => assert.equal(text, 'NZD'));
 
-    // it('should show Invalid Input if any Currency input length is not 3', () => {});
+        cy.get('input[ng-reflect-placeholder=To]')
+            .invoke('val')
+            .then(text => assert.equal(text, 'USD'));
 
-    // it('should show Invalid Input if any Currency input is not an exited currency', () => {});
+        cy.get('input[type=number]')
+            .invoke('val')
+            .then(text => assert.equal(text, '1'));
+    });
 
-    // it('should show Invalid Input if any Currency input is not an exited currency', () => {});
+    it('should show Invalid Input if any currency input length is not 3', () => {
+        cy.get('input[ng-reflect-placeholder=From]')
+            .clear()
+            .then(() => {
+                cy.get('.converter__result').should('contain.text', 'Invalid Input');
+            });
+    });
+
+    it('should show Invalid Input if any currency input is not an exited currency', () => {
+        cy.get('input[ng-reflect-placeholder=From]')
+            .clear()
+            .type('NZZ')
+            .then(() => {
+                cy.get('.converter__result').should('contain.text', 'Invalid Input');
+            });
+    });
+
+    it('should show result if inputs are valid', () => {
+        cy.get('input[ng-reflect-placeholder=To]')
+            .clear()
+            .type('AUD')
+            .then(() => {
+                cy.get('.converter__result').should('contain.text', 'AUD');
+            });
+    });
 });
